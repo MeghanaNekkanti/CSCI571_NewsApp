e28dd810ec9c1e00ca380a99768463226e8bb9f4
Meghana Nekkanti <mnekkant@usc.edu>
2020-04-18 23:50:29 -0700

    Trending fragment UI, Line chart implemented

---
+++ src/main/java/com/example/newsapp/ui/trending/TrendingFragment.java
@@ -0,0 +1,65 @@
+package com.example.newsapp.ui.trending;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.newsapp.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class TrendingFragment extends Fragment {
+
+    LineChart lineChart;
+    LineData lineData;
+    LineDataSet lineDataSet;
+    ArrayList lineEntries;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_trending, container, false);
+        lineChart = root.findViewById(R.id.lineChart);
+        getEntries();
+        lineDataSet = new LineDataSet(lineEntries, "Trending Chart for Coronavirus");
+        lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineDataSet.setColors(R.color.design_default_color_primary);
+//        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(10f);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.getAxisRight().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawAxisLine(false);
+        Legend legend = lineChart.getLegend();
+        legend.setFormSize(10f); // set the size of the legend forms/shapes
+        return root;
+
+    }
+
+    private void getEntries() {
+        lineEntries = new ArrayList<>();
+        lineEntries.add(new Entry(2f, 0));
+        lineEntries.add(new Entry(4f, 1));
+        lineEntries.add(new Entry(6f, 1));
+        lineEntries.add(new Entry(8f, 3));
+        lineEntries.add(new Entry(7f, 4));
+        lineEntries.add(new Entry(3f, 3));
+    }
+
+}
