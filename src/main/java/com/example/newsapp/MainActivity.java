302ef0bdb3748349e35e2878d3daec58c08ab3d9
Meghana Nekkanti <mnekkant@usc.edu>
2020-04-20 02:59:17 -0700

    Location Permission and Weather card details using Openweathermap done

--- src/main/java/com/example/newsapp/MainActivity.java
+++ src/main/java/com/example/newsapp/MainActivity.java
@@ -1,16 +1,48 @@
 package com.example.newsapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.newsapp.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    //    HomeFragment homeFragment = new HomeFragment();
+    Boolean display = false;
+    private final int REQUEST_LOCATION_PERMISSION = 1;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    double longitude;
+    double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,4 +60,104 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        Log.d("TAG", "onRequestPermissionsResult: ");
+//        // Forward results to EasyPermissions
+//
+//        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+//    }
+//
+//    @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
+//    public void requestLocationPermission() {
+//        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
+//        if (EasyPermissions.hasPermissions(this, perms)) {
+//            setDisplay(true);
+//            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return;
+//            }
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+//            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            Log.d("TAG", "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
+//
+//            setLatitude(location.getLatitude());
+//            setLongitude(location.getLongitude());
+//            Log.d("TAG", "requestLocationPermission: 1");
+//
+//            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
+//            FragmentManager manager = getSupportFragmentManager();
+//            Fragment fragment = manager.findFragmentById(R.id.nav_host_fragment);
+//            Log.d("TAG", "requestLocationPermission: "+fragment);
+//            HomeFragment homeFragment = new HomeFragment();
+//            homeFragment.setMyView(getLatitude(),getLongitude(), this,fragment.getView()
+//            );
+//        } else {
+//            Log.d("TAG", "requestLocationPermission: 2");
+//            EasyPermissions.requestPermissions(this, "Please grant the location permission", REQUEST_LOCATION_PERMISSION, perms);
+//        }
+//    }
+
+//    public Boolean getDisplay() {
+//        Log.d("TAG", "getDisplay: ");
+//        return display;
+//    }
+//
+//    public double getLatitude() {
+//        Log.d("TAG", "getLatitude: ");
+//        return latitude;
+//    }
+//
+//    public double getLongitude() {
+//        Log.d("TAG", "getLongitude: ");
+//        return longitude;
+//    }
+//
+//    public void setLatitude(double latitude) {
+//        Log.d("TAG", "setLatitude: ");
+//        this.latitude = latitude;
+//    }
+//
+//    public void setLongitude(double longitude) {
+//        Log.d("TAG", "setLongitude: ");
+//        this.longitude = longitude;
+//    }
+//
+//    public void setDisplay(Boolean display) {
+//        Log.d("TAG", "setDisplay: ");
+//        this.display = display;
+//    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+//        Log.d("TAG", "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
+//        setLatitude(location.getLatitude());
+//        setLongitude(location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
+
+
+
--- src/main/java/com/example/newsapp/ui/home/HomeFragment.java
+++ src/main/java/com/example/newsapp/ui/home/HomeFragment.java
@@ -1,20 +1,54 @@
 package com.example.newsapp.ui.home;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.newsapp.MainActivity;
 import com.example.newsapp.R;
 import com.example.newsapp.adapter.NewsAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 import static androidx.recyclerview.widget.RecyclerView.HORIZONTAL;
 import static androidx.recyclerview.widget.RecyclerView.VERTICAL;
@@ -24,30 +58,187 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     NewsAdapter newsAdapter;
     ArrayList<String> items;
+    View root;
+    private final int REQUEST_LOCATION_PERMISSION = 1;
+    protected LocationManager locationManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        items = new ArrayList<>();
-        items.add("First");
-        items.add("Second");
-        items.add("Third");
-        items.add("Four");
-        items.add("Five");
-        items.add("Six");
-
-        DividerItemDecoration itemDecor = new DividerItemDecoration(getContext(), HORIZONTAL);
-        itemDecor.setOrientation(VERTICAL);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerView = root.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        newsAdapter = new NewsAdapter(getActivity(), items, "Home");
-        recyclerView.addItemDecoration(itemDecor);
-        recyclerView.setAdapter(newsAdapter);
-//        final TextView textView = root.findViewById(R.id.text_state);
-//
-//        textView.setText("s");
-//
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+        Log.d("TAG", "onCreateView: " + "loc");
+        if (savedInstanceState == null)
+            root.findViewById(R.id.weather_card).setVisibility(View.GONE);
+        requestLocationPermission();
         return root;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
+    public void requestLocationPermission() {
+        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if (EasyPermissions.hasPermissions(getContext(), perms)) {
+            Toast.makeText(getContext(), "Permission already granted", Toast.LENGTH_SHORT).show();
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) getActivity());
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Log.d("TAG", "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
+
+            Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                Log.d("TAG", "onResume: " + addresses);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String cityName = addresses.get(0).getLocality();
+            String stateName = addresses.get(0).getAdminArea();
+//            String countryName = addresses.get(0).getAddressLine(2);
+            Log.d("TAG", "onResume: " + cityName + " " + stateName);
+
+            TextView state = root.findViewById(R.id.text_state);
+            TextView city = root.findViewById(R.id.text_city);
+            state.setText(stateName);
+            city.setText(cityName);
+            final TextView temperature = root.findViewById(R.id.text_temp);
+            final TextView summary = root.findViewById(R.id.text_summary);
+            final ImageView back = root.findViewById(R.id.background);
+
+            RequestQueue queue = Volley.newRequestQueue(getActivity());
+            String api_key = "b4458e11d6e5b81dd5441fa4250e9879";
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + api_key;
+
+            JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Display the first 500 characters of the response string.
+                            try {
+                                JSONObject jsonObject0 = response.getJSONObject("main");
+                                JSONArray ja = response.getJSONArray("weather");
+                                JSONObject jsonObject = ja.getJSONObject(0);
+                                String main = jsonObject.getString("main");
+                                Integer temp = Math.round(Float.parseFloat(jsonObject0.getString("temp")));
+                                temperature.setText(temp + " \u2103");
+                                summary.setText(main);
+                                switch (main){
+                                    case "Clouds":
+                                        back.setBackground(getResources().getDrawable(R.drawable.cloudy_weather));
+                                        break;
+                                    case "Clear":
+                                        back.setBackground(getResources().getDrawable(R.drawable.clear_weather));
+                                        break;
+                                    case "Snow":
+                                        back.setBackground(getResources().getDrawable(R.drawable.snowy_weather));
+                                        break;
+                                    case "Rain":
+                                    case "Drizzle":
+                                        back.setBackground(getResources().getDrawable(R.drawable.rainy_weather));
+                                        break;
+                                    case "Thunderstorm":
+                                        back.setBackground(getResources().getDrawable(R.drawable.thunder_weather));
+                                        break;
+                                    default:
+                                        back.setBackground(getResources().getDrawable(R.drawable.sunny_weather));
+                                        break;
+                                }
+
+                                Log.d("TAG", "onResponse: " + response);
+                                Log.d("TAG", "onResponse: " + main + temp);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+//                            textView.setText("Response is: " + response.substring(0, 500));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Volley", "Error");
+
+                }
+            });
+
+            queue.add(jsonRequest);
+            DividerItemDecoration itemDecor = new DividerItemDecoration(getActivity(), HORIZONTAL);
+            itemDecor.setOrientation(VERTICAL);
+//        Log.d("TAG", "onCreateView: " + "loc2");
+            items = new ArrayList<>();
+            items.add("First");
+            items.add("Second");
+            items.add("Third");
+            items.add("Four");
+            items.add("Five");
+            items.add("Six");
+            recyclerView = root.findViewById(R.id.recyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            newsAdapter = new NewsAdapter(getActivity(), items, "Home");
+//        Log.d("TAG", "onCreateView: " + "loc");
+            recyclerView.addItemDecoration(itemDecor);
+            recyclerView.setAdapter(newsAdapter);
+
+            root.findViewById(R.id.weather_card).setVisibility(View.VISIBLE);
+        } else {
+            EasyPermissions.requestPermissions(this, "Please grant the location permission", REQUEST_LOCATION_PERMISSION, perms);
+        }
+    }
+//
+//    public void setMyView(Double latitude, Double longitude, MainActivity mainActivity, View view) {
+////        MainActivity mainActivity = (MainActivity) getActivity();
+////        Double longitude = mainActivity.getLongitude();
+////        Double latitude = mainActivity.getLatitude();
+//        Log.d("TAG", "onResume: " + longitude + latitude + " " + context);
+//        Geocoder geocoder = new Geocoder(mainActivity, Locale.getDefault());
+//        List<Address> addresses = null;
+//        try {
+//            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+//            Log.d("TAG", "onResume: " + addresses);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String cityName = addresses.get(0).getLocality();
+//        String stateName = addresses.get(0).getAdminArea();
+////            String countryName = addresses.get(0).getAddressLine(2);
+//        Log.d("TAG", "onResume: " + cityName + " " + stateName);
+//
+//        Log.d("TAG", "onResume: ");
+//
+//        DividerItemDecoration itemDecor = new DividerItemDecoration(mainActivity, HORIZONTAL);
+//        itemDecor.setOrientation(VERTICAL);
+////        Log.d("TAG", "onCreateView: " + "loc2");
+//        items = new ArrayList<>();
+//        items.add("First");
+//        items.add("Second");
+//        items.add("Third");
+//        items.add("Four");
+//        items.add("Five");
+//        items.add("Six");
+//        recyclerView = view.findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(mainActivity));
+//        newsAdapter = new NewsAdapter(mainActivity, items, "Home");
+////        Log.d("TAG", "onCreateView: " + "loc");
+//        recyclerView.addItemDecoration(itemDecor);
+//        recyclerView.setAdapter(newsAdapter);
+//        TextView textView = view.findViewById(R.id.text_state);
+//        textView.setText("STATE");
+//        view.findViewById(R.id.weather_card).setVisibility(View.VISIBLE);
+//
+//    }
 }
